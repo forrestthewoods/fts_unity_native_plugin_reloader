61c295f35589a1a7ac8257dda9037b16a7833247
Bence Kovács <23636204+githappens@users.noreply.github.com>
2023-10-07 10:44:56 +0200

    make dll example compatible on macOS and add CMakeLists

--- cpp_example_dll/c_api.h
+++ cpp_example_dll/c_api.h
@@ -1,19 +1,38 @@
 #pragma once
+#ifndef UNITY_INTERFACE_API
+    #if defined(_MSC_VER)
+        #define UNITY_INTERFACE_API __stdcall
+    #elif defined(__GNUC__)
+        #define UNITY_INTERFACE_API
+    #endif
+#endif
+
+#ifndef UNITY_INTERFACE_EXPORT
+    #if defined(_MSC_VER)
+        #define UNITY_INTERFACE_EXPORT __declspec(dllexport)
+    #elif defined(__GNUC__)
+        #define UNITY_INTERFACE_EXPORT __attribute__ ((visibility ("default")))
+    #endif
+#endif
 
 // API must be "strict" C
 // It is STRONGLY encourage that these functions should NEVER
 // crash, throw, abort, or terminate.
+#ifdef __cplusplus
 extern "C" 
 {
-	__declspec(dllexport) int simple_func();
-	__declspec(dllexport) float sum(float a, float b);
-	__declspec(dllexport) int string_length(const char* s);
+#endif
+	UNITY_INTERFACE_EXPORT int UNITY_INTERFACE_API simple_func();
+	UNITY_INTERFACE_EXPORT float UNITY_INTERFACE_API sum(float a, float b);
+	UNITY_INTERFACE_EXPORT int UNITY_INTERFACE_API string_length(const char* s);
 
 	struct SimpleStruct {
 		int a;
 		float b;
 		bool c;
 	};
-	__declspec(dllexport) double send_struct(SimpleStruct const* ss);
-	__declspec(dllexport) SimpleStruct recv_struct();
+	UNITY_INTERFACE_EXPORT double UNITY_INTERFACE_API send_struct(SimpleStruct const* ss);
+	UNITY_INTERFACE_EXPORT SimpleStruct UNITY_INTERFACE_API recv_struct();
+#ifdef __cplusplus
 }
+#endif
